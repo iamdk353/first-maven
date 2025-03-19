8b1048806e53af50184f6cb2aaac5dd9065a66c9
Dhanush Kumar M <iamdk353@gmail.com>
2025-03-19 12:27:33 +0530

    push 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world this is a build test");
     }
 }
